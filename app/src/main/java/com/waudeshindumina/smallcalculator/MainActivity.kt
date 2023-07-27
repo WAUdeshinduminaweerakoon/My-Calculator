fe58afb042eb3596fafd4f0ba75e87576dc61e66
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-07-27 20:46:22 +0530

    Added function in clear button

--- app/src/main/java/com/waudeshindumina/smallcalculator/MainActivity.kt
+++ app/src/main/java/com/waudeshindumina/smallcalculator/MainActivity.kt
@@ -3,6 +3,9 @@ package com.waudeshindumina.smallcalculator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +14,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickDigit(view: View) {
+        InputText.append((view as Button).text)
+
+    }
+    fun onClear(view: View){
+        InputText.text = ""
 
     }
 }
